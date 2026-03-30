8cd33c30132754b8acccf962b98740eba73c2fda
elf Pavlik <elf-pavlik@hackers4peace.net>
2026-03-29 18:28:25 -0600

    first snippets

--- src/model/styles.ts
+++ src/model/styles.ts
@@ -1,3 +1,6 @@
+import { tags as t } from '@lezer/highlight'
+import { HighlightStyle } from '@codemirror/language'
+
 export const shadow = {
   shadowColor: "black",
   shadowOffset: 5,
@@ -9,4 +12,35 @@ export const shapes = {
   square: { sides: 4, color: '#40C4FF' },
   pentagon: { sides: 5, color: '#4ED8A0' },
   hexagon: { sides: 6, color: '#7C4DFF' }
-};
+}
+
+const mocha = {
+  text: '#cdd6f4',
+  subtext1: '#bac2de',
+  surface1: '#45475a',
+
+  blue: '#89b4fa',
+  mauve: '#cba6f7',
+  green: '#a6e3a1',
+  yellow: '#f9e2af',
+  red: '#f38ba8',
+  peach: '#fab387',
+  teal: '#94e2d5',
+}
+
+export const CatppuccinMocha = HighlightStyle.define([
+  { tag: t.keyword, color: mocha.mauve },
+  { tag: t.string, color: mocha.green },
+  { tag: t.number, color: mocha.peach },
+  { tag: t.bool, color: mocha.peach },
+
+  { tag: t.propertyName, color: mocha.blue },
+  { tag: t.variableName, color: mocha.text },
+
+  { tag: t.comment, color: mocha.surface1, fontStyle: 'italic' },
+
+  { tag: t.typeName, color: mocha.yellow },
+  { tag: t.operator, color: mocha.teal },
+
+  { tag: t.punctuation, color: mocha.subtext1 },
+])
--- src/scenes/example.ts
+++ src/scenes/example.ts
@@ -1,8 +1,12 @@
-import { Node, Camera, makeScene2D } from '@motion-canvas/2d';
-import { beginSlide, Vector2, all } from '@motion-canvas/core';
+import { Node, Camera, makeScene2D, Code, LezerHighlighter, Rect, lines } from '@motion-canvas/2d';
+import { beginSlide, Vector2, all, DEFAULT } from '@motion-canvas/core';
+import { parser } from '@lezer/yaml';
 import { actors, makeActor } from '../model/actors';
 import { makeStorage } from '../model/storage';
 import { makeGrant } from '../model/grants';
+import { CatppuccinMocha } from '../model/styles';
+
+Code.defaultHighlighter = new LezerHighlighter(parser, CatppuccinMocha);
 
 function grid(x: number, y: number): [number, number] {
   return [x * 300, y * 300]
@@ -52,13 +56,67 @@ export default makeScene2D(function* (view) {
   // })
   // arrow.points([[50, 250], [50, 250]])
 
+  const code = new Code({
+    fontSize: 32,
+    code: `\
+"@context": https://...
+grantee: Max
+grantedBy: SME
+dataOwner: SME
+hasStorage: https://eu.sme.ex
+registeredShapeTree: TaxFilings 🔷
+scopeOfGrant: AllFromRegistry
+accessMode: [ Read, Create ]`,
+  })
+
+  const snippetS2M = new Rect({
+    layout: true,
+    padding: 32 * 1.5,
+    radius: 12,
+    fill: '#1e1e2e',
+    stroke: '#585b70',
+    lineWidth: 4,
+    children: code
+  });
+  snippetS2M.position(grid(6, 0.5))
+
+  const code2 = new Code({
+    fontSize: 32,
+    code: `\
+"@context": https://...
+grantee: App
+grantedBy: Max
+dataOwner: SME
+hasStorage: https://eu.sme.ex
+registeredShapeTree: TaxFilings 🔷
+scopeOfGrant: SelectedFromRegistry
+hasDataInstance:
+  - https://eu.sme.ex/june25
+  - https://eu.sme.ex/july25
+accessMode: [ Read ]`,
+  })
+
+  const snippetS2M2A = new Rect({
+    layout: true,
+    padding: 32 * 1.5,
+    radius: 12,
+    fill: '#1e1e2e',
+    stroke: '#585b70',
+    lineWidth: 4,
+    children: code2
+  });
+  snippetS2M2A.position(grid(6, 0.75))
+
+
   const width = 1920;
   const height = 1080;
+  const elements = [sme, acme, max, app, smeStorage, acmeStorage, grantS2M, grantA2M, grantS2M2A, grantA2M2A]
+  const snippets = [snippetS2M, snippetS2M2A]
 
   const scene = new Node({
     position: new Vector2(width / 2, height / 2),
     cachePadding: [width, height, width, height],
-    children: [sme, acme, max, app, smeStorage, acmeStorage, grantS2M, grantA2M, grantS2M2A, grantA2M2A],
+    children: [...elements, ...snippets]
   });
 
   const camera = new Camera({
@@ -102,6 +160,54 @@ export default makeScene2D(function* (view) {
     grantA2M2A.position(grid(-1, 1), 1)
     // arrow.opacity(1, 1),
     // arrow.points([[250, 50], [50, 250]], 1),
+  )
+  yield* beginSlide('Snippet')
+  yield* all(
+    camera.centerOn(grid(1, 0), 1),
+    ...elements.filter(el => el !== grantS2M).map(el => el.opacity(0.5, 1)),
+    snippetS2M.position(grid(2.75, 0.5), 1),
+  )
+  yield* beginSlide('Agents')
+  yield* all(
+    max.opacity(1, 1),
+    sme.opacity(1, 1),
+    code.selection(lines(1, 3), 1),
+  )
+  yield* beginSlide('scope')
+  yield* all(
+    max.opacity(0.5, 1),
+    sme.opacity(0.5, 1),
+    smeStorage.opacity(1, 1),
+    code.selection(lines(4, 7), 1),
+  )
+  yield* beginSlide('Reset')
+  yield* all(
+    smeStorage.opacity(0.5, 1),
+    code.selection(DEFAULT, 1)
+  )
+  yield* beginSlide('Snippet 2')
+  yield
+  yield* all(
+    snippetS2M.position(grid(2.75, -1), 0.5),
+  )
+  yield* all(
+    grantS2M2A.opacity(1, 1),
+    snippetS2M2A.position(grid(2.75, 0.75), 1),
+  )
+  yield* beginSlide('Compare Agents')
+  yield* all(
+    max.opacity(1, 1),
+    app.opacity(1, 1),
+    code.selection(lines(1, 3), 1),
+    code2.selection(lines(1, 3), 1),
+  )
+  yield* beginSlide('Compare Scope')
+  yield* all(
+    max.opacity(0.5, 1),
+    app.opacity(0.5, 1),
+    smeStorage.opacity(1, 1),
+    code.selection(lines(6, 7), 1),
+    code2.selection(lines(6, 10), 1),
   )
   yield* beginSlide('End')
 });
